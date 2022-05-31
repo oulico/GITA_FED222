2823bf54ce4832d63813f68633de42e5d76b2fb3
manemis <manemis@gmail.com>
2022-05-31 17:19:48 +0900

    dd

--- 001.HTML학습/07.CGV_PJ/003.site_v1/js/main.js
+++ 001.HTML학습/07.CGV_PJ/003.site_v1/js/main.js
@@ -15,5 +15,5 @@ function chgMV(vid) {
 
   // 3. 변경내용: src 속성 재설정하기
   // 선택요소.src = 변경할 값
-  tg.src = `https://www.youtube.com/embed/${vid}?autoplay=1&loop=1&playlist=${vid}&playsinline=1`;
+  tg.src = `https://www.youtube.com/embed/${vid}?autoplay=1&playsinline=1`;
 } // chgMV 함수 //
