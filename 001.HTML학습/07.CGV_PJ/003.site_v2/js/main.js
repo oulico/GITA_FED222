c697907df25f0b471a33460e5fcde1ccade154c2
manemis <manemis@gmail.com>
2022-06-10 15:02:23 +0900

    error

---
+++ 001.HTML학습/07.CGV_PJ/003.site_v2/js/main.js
@@ -0,0 +1,55 @@
+// CGV main 페이지 js - main.js //
+
+////////////// 로드구역 //////////////
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("로딩완료");
+
+  ///// 영화선택메뉴 클릭시 기능구현 정의 /////
+  //1. 영화선택  메뉴 클릭시 클릭된 li에
+  // 클래스 on을 추가한다.
+  // 2. 나머지 영화선택 메뉴 li에는 클래스 "on"을 삭제ㅐ
+  // -> 결과적으로 css로 구현한 선택메뉴 일어서 있기가 구현된다.
+
+  // 이벤트 대상 : .mlist ul > li
+
+  //변경 대상 === 이벤트 대상 (this)
+  // 이벤트 종류 : click
+  // 변경대상이 여러개 이므로 for of 문을 사용한다.
+
+  let sml = document.querySelectorAll(".mlist ul > li");
+
+  console.log("영화선택리스트", sml);
+
+  // 선택요소 들의 노드리스트 컬렉션을 모두 click설정함
+
+  for (let x of sml) {
+    // x는 각각의 li 요소다.
+
+    x.onclick = () => {
+      // 모든 요소 클래스 on 지우기
+      for (let z of sml) z.classList.remove("on");
+
+      // 해당요소에 클래스 넣기
+      // classList 의 메서드 add()
+
+      x.classList.add("on");
+    };
+  }
+});
+
+/**************************************** 
+   함수명: chgMV
+   기능: 영화 예고편 선택하여 플레이하기 
+****************************************/
+function chgMV(vid) {
+  //vid - 동영상id
+
+  // 1. 함수 호출 확인 + 전달값 확인
+  console.log("바꿔", vid);
+  // 2. 변경 대상 선정
+  var tg = document.querySelector("#screen iframe");
+
+  // 3. 변경내용: src 속성 재설정하기
+  // 선택요소.src = 변경할 값
+  tg.src = `https://www.youtube.com/embed/${vid}?autoplay=1&playsinline=1`;
+} // chgMV 함수 //
